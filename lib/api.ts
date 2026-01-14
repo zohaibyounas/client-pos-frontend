99b39fbcf60b45acfbf10bdea4f9a8728e8be5fd
zohaibyounas <zohaiby737@gmail.com>
2026-01-14 17:50:18 +0500

    updated

--- lib/api.ts
+++ lib/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: " https://client-pos-backend.onrender.com/api",
+  baseURL: " https://client-pos-backend.onrender.com/auth/api",
   headers: {
     "Content-Type": "application/json",
   },
