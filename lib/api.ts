6a0c4f396c3e3f4ca8bfa7749eaeb7ab15a4da15
zohaibyounas <zohaiby737@gmail.com>
2026-01-14 17:48:01 +0500

    updated

--- lib/api.ts
+++ lib/api.ts
@@ -1,24 +1,24 @@
-import axios from 'axios';
+import axios from "axios";
 
 const api = axios.create({
-    baseURL: 'https://8960db44-d386-48bc-904e-499f3d8fdee2-00-1wlaoh0fovpl5.pike.replit.dev/api',
-    headers: {
-        'Content-Type': 'application/json',
-    },
+  baseURL: " https://client-pos-backend.onrender.com/api",
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
-
+//dddd
 // Add a request interceptor
 api.interceptors.request.use(
-    (config) => {
-        const token = localStorage.getItem('token');
-        if (token) {
-            config.headers.Authorization = `Bearer ${token}`;
-        }
-        return config;
-    },
-    (error) => {
-        return Promise.reject(error);
+  (config) => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
     }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
 );
 
 export default api;
